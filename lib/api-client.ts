1a2f3abb39d4f1effacedf423fa064f332c83cd6
kipko-sudo <kipkorirv53@gmail.com>
2026-03-19 10:45:31 +0300

    enroll, module status, quiz

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -7,7 +7,7 @@ import axios, { AxiosInstance, AxiosError, AxiosResponse } from 'axios';
 import Cookies from 'js-cookie';
 
 // API Configuration
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://lionschool.pythonanywhere.com/api'; //https://lionschool.pythonanywhere.com/
 
 /**
  * Create and configure axios instance
